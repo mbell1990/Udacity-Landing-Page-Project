196375c6408d965534b953cd5f5728bf3082da73
Matthew Bell <m.r.bell@outlook.com>
2022-06-09 07:26:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Udacity Landing Page Project
+
+## Description
+
+This is the second project completed as part of the Udacity Front End Nanodegree. The aim of this project was to create a landing page with multiple page sections which implemented some core JavaScript behaviours such as DOM manipulation, smooth scrolling and identfying sections being viewed by the user.
+
+## Languages used
+
+- This project uses HTML, CSS and vanilla JavaScript
+- The HTML and CSS code was created using the template Udacity provided
+- JavaScript code is my own
+
+## Sources used
+
+- https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+- https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
---
+++ js/app.js
@@ -0,0 +1,122 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+"use strict";
+
+const navbar = document.getElementById("navbar__list");
+const pageSections = document.querySelectorAll("section");
+const btn = document.getElementById("scrollBtn");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+function navigation() {
+  pageSections.forEach((section) => {
+    let li = document.createElement("li");
+    li.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+    navbar.appendChild(li);
+  });
+}
+
+navigation();
+
+// Add class 'active' to section when near top of viewport
+
+// helper function to determine if section is in viewport.
+let sectionInViewport = function (elem) {
+  let bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// event to listner to look out for scroll. loop sections to asses if on scroll it is in viewport then add active class.
+
+window.addEventListener("scroll", () => {
+  pageSections.forEach((section) => {
+    if (sectionInViewport(section)) {
+      section.classList.add("your-active-class");
+    } else {
+      section.classList.remove("your-active-class");
+    }
+  });
+});
+
+// Scroll to anchor ID using scrollTO event
+
+navbar.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("menu__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// scroll to top using button
+
+window.onscroll = function () {
+  btnScroll();
+};
+
+function btnScroll() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
